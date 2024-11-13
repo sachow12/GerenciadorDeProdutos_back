b66c3558f7444a78c6b45f64e72b04551a6c6170
sachow12 <salomaovasques2@gmail.com>
2024-11-12 21:16:44 -0300

    UPDATE FUNCIONANDO e filtro adicionado

--- src/main/java/elevar/gerenciamento/Controller/ProductController.java
+++ src/main/java/elevar/gerenciamento/Controller/ProductController.java
@@ -1,17 +1,23 @@
 package elevar.gerenciamento.Controller;
 
 import elevar.gerenciamento.Model.Product;
+import elevar.gerenciamento.Repository.ProductRepository;
 import elevar.gerenciamento.Service.ProductService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
 
     @PostMapping
     public Product create(@RequestBody Product product) {
@@ -28,5 +34,25 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         productService.deleteProduct(id);
+    }
+
+    @GetMapping("/filter")
+    public Page<Product> filterProducts(
+            @RequestParam(required = false) String codigo,
+            @RequestParam(required = false) String descricao,
+            @RequestParam(required = false) Double precoPromocional,
+            @RequestParam(required = false) Boolean promocao,
+            @RequestParam(required = false) Integer qtdEstoque,
+            @RequestParam(required = false) Boolean status,
+            @RequestParam(required = false) String titulo,
+            @RequestParam(required = false) Double valor,
+            @RequestParam(required = false) String referencia,
+            @RequestParam(required = false) Boolean destaque,
+            @RequestParam(required = false) List<String> imgTopo,
+            Pageable pageable) {
+
+        return productService.filterProducts(
+                codigo, descricao, precoPromocional, promocao, qtdEstoque, status,
+                titulo, valor, referencia, destaque, imgTopo, pageable);
     }
 }
